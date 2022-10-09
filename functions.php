9d97eb0d0f05bde9ba1d3c62f249e5da4440969b
java-chip <takuyam0111@yahoo.co.jp>
2022-10-10 02:12:15 +0900

    テーマ独自のjavascript読み込み

--- functions.php
+++ functions.php
@@ -1,5 +1,22 @@
 <?php
+// ページごとのタイトル自動出力
 function neko_theme_setup() {
     add_theme_support('title-tag');
 }
+
+// neko_theme_setupを、after_setup_themeのタイミングで実行
 add_action('after_setup_theme', 'neko_theme_setup');
+
+// テーマ独自のJavascript・CSS読み込み
+function neko_enqueue_scripts() {
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('kuroneko-theme-common',
+                       get_template_directory_uri() . '/assets/js/theme-common.js',
+                       array(),
+                       '1.0.0',
+                       true
+                    );
+}
+
+add_action('wp_enqueue_scripts', 'neko_enqueue_scripts');
+?>
